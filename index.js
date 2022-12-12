a52cb332fc7540e7b6d29747e530acf34d62b11b
Jeaustin <JeaustinS27@gmail.com>
2022-12-11 22:44:51 -0500

    added packages and added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./assets/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =  [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why, and how of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What license does your project have?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can someone contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What needs to be done to run tests?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email in case someone needs to reach you with questions?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
